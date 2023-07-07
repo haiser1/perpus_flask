626857e656c48f3741f47d5075393abdbd8d40f1
haiser1 <115344060+haiser1@users.noreply.github.com>
2023-07-08 01:02:37 +0700

    Delete routes directory

--- routes/user/route.py
+++
@@ -1,5 +0,0 @@
-from flask import render_template, jsonify, request, session, Blueprint
-from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime, timedelta
-from connection_db.data_base import db
-user_bf = Blueprint('user_bf', __name__)
